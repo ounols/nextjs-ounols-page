2ff3b00a9dddac53680fa1da430f2de25c9ceaeb
이진영 <ounols@naver.com>
2022-09-14 17:02:24 +0900

    스타일 고정

--- site.config.ts
+++ site.config.ts
@@ -2,26 +2,26 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'f76197a2521241d99dc6de8dfd6b6b4c',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'ounols',
+  domain: 'ounols.kr',
+  author: 'ounols',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'ounols.kr',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  // twitter: 'transitive_bs',
+  github: 'ounols',
+  // linkedin: 'fisch2',
   // newsletter: '#', // optional newsletter URL
-  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+  youtube: 'ounols', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
@@ -45,7 +45,11 @@ export default siteConfig({
   //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
   //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
   // }
-  pageUrlOverrides: null,
+  pageUrlOverrides: {
+    '/ko': '28b6b04bbda740cc9f279b2874cdf46f',
+    '/CSEngine-en': '0ac013d495534455a7742ee7928742db',
+    '/Angpang-en': '520e130e40d340b8a9f856abced64f3d',
+  },
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
