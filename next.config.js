9d650ee969b9ade3b55d5bebc9c9851d29aae81e
ounols <ounols@naver.com>
2022-09-15 00:37:12 +0900

    🥚

--- next.config.js
+++ next.config.js
@@ -17,7 +17,5 @@ module.exports = withBundleAnalyzer({
     formats: ['image/avif', 'image/webp'],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    loader: 'akamai',
-    path: '',
   }
 })
