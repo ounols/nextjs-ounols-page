de72ce8ece13ba0b7faca3ea0a4706496302222f
ounols <ounols@naver.com>
2025-03-02 01:29:15 +0900

    Add ads.txt

---
+++ pages/ads.txt.tsx
@@ -0,0 +1,34 @@
+import type { GetServerSideProps } from 'next'
+
+import { host } from '@/lib/config'
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  if (req.method !== 'GET') {
+    res.statusCode = 405
+    res.setHeader('Content-Type', 'application/json')
+    res.write(JSON.stringify({ error: 'method not allowed' }))
+    res.end()
+
+    return {
+      props: {}
+    }
+  }
+
+  // cache for up to one day
+  res.setHeader('Cache-Control', 'public, max-age=86400, immutable')
+  res.setHeader('Content-Type', 'text/plain')
+
+  res.write(
+    `google.com, pub-7885608228730680, DIRECT, f08c47fec0942fa0`
+  )
+
+  res.end()
+
+  return {
+    props: {}
+  }
+}
+
+export default function noop() {
+  return null
+}
